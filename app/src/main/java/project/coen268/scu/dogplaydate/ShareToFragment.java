9be799dbb3fd6fdfc83e680cab155e2339e29108
feliciafay007 <feliciafay@gmail.com>
2015-06-03 18:43:56 -0700

    UI step 2

--- app/src/main/java/project/coen268/scu/dogplaydate/ShareToFragment.java
+++ app/src/main/java/project/coen268/scu/dogplaydate/ShareToFragment.java
@@ -2,6 +2,7 @@ package project.coen268.scu.dogplaydate;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,9 +19,23 @@ public class ShareToFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         View rootView = inflater.inflate(R.layout.fragment_share, container, false);
 
+//        Intent in = new Intent(getActivity(),Login.class);
+//        //System.out.println("back to login");
+//        startActivity(in);
+
+
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = "Here is a nice app that we can use together";
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Hi, Let's use DogPlayDate");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+
         return rootView;
+
     }
 }
 
